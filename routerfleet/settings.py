f364913ebbed59636b22cdda9068f919b38f85bd
Eduardo Silva <eduardo@eth0.com.br>
2024-04-12 15:20:13 -0300

    Allow SSH connection to different ports. Fixed issue with backup schedule generator

--- routerfleet/settings.py
+++ routerfleet/settings.py
@@ -140,6 +140,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = '/var/lib/routerfleet/'
-ROUTERFLEET_VERSION = 7008
+ROUTERFLEET_VERSION = 7009
 
 from routerfleet.production_settings import *
