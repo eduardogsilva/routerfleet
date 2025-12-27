b6103dc4da5c21903f4fe5f32ce8ffea4cbdfc21
Eduardo Silva <eduardo@eth0.com.br>
2025-12-27 18:20:32 -0300

    bump ROUTERFLEET_VERSION to 7505

--- routerfleet/settings.py
+++ routerfleet/settings.py
@@ -142,6 +142,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_ROOT = '/var/lib/routerfleet/'
-ROUTERFLEET_VERSION = 7504
+ROUTERFLEET_VERSION = 7505
 
 from routerfleet.production_settings import *
