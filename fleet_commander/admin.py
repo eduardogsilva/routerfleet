5bcfdda50e709d7ea81dd791e0714578d8ab86b6
Eduardo Silva <eduardo@eth0.com.br>
2026-02-20 20:43:32 -0300

    add admin interfaces for Command, CommandVariant, CommandSchedule, CommandJob, and CommandTask models

--- fleet_commander/admin.py
+++ fleet_commander/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Command, CommandVariant, CommandSchedule, CommandJob, CommandTask
+
+
+class CommandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'created', 'updated')
+    search_fields = ('name', 'description')
+    list_filter = ('enabled', 'created', 'updated')
+admin.site.register(Command, CommandAdmin)
+
+class CommandVariantAdmin(admin.ModelAdmin):
+    list_display = ('command', 'router_type', 'enabled', 'created', 'updated')
+    search_fields = ('command__name', 'router_type')
+    list_filter = ('enabled', 'router_type', 'created', 'updated')
+admin.site.register(CommandVariant, CommandVariantAdmin)
+
+class CommandScheduleAdmin(admin.ModelAdmin):
+    list_display = ('command', 'enabled', 'next_run', 'created', 'updated')
+    search_fields = ('command__name',)
+    list_filter = ('enabled', 'next_run', 'created', 'updated')
+admin.site.register(CommandSchedule, CommandScheduleAdmin)
+
+class CommandJobAdmin(admin.ModelAdmin):
+    list_display = ('command', 'exec_source', 'user_source_name', 'task_count', 'success_count', 'error_count', 'created', 'updated')
+    search_fields = ('command__name', 'user_source_name')
+    list_filter = ('exec_source', 'created', 'updated')
+admin.site.register(CommandJob, CommandJobAdmin)
+
+class CommandTaskAdmin(admin.ModelAdmin):
+    list_display = ('job', 'router', 'status', 'retry_count', 'created', 'updated')
+    search_fields = ('job__command__name', 'router__name')
+    list_filter = ('status', 'created', 'updated')
+admin.site.register(CommandTask, CommandTaskAdmin)
