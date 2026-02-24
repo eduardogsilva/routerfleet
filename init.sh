237bf8553627dd1eee487a86bdedd82810af1602
Eduardo Silva <eduardo@eth0.com.br>
2026-02-24 09:15:09 -0300

    update gunicorn configuration

--- init.sh
+++ init.sh
@@ -7,4 +7,15 @@ shopt -s nullglob
 # Django startup
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-exec gunicorn routerfleet.wsgi:application --bind 0.0.0.0:8001 --workers 2 --threads 2 --timeout 55 --log-level info --capture-output --access-logfile - --error-logfile -
+exec gunicorn routerfleet.wsgi:application \
+  --bind 0.0.0.0:8001 \
+  --worker-class sync \
+  --workers 5 \
+  --timeout 300 \
+  --graceful-timeout 30 \
+  --max-requests 1000 \
+  --max-requests-jitter 200 \
+  --log-level info \
+  --capture-output \
+  --access-logfile - \
+  --error-logfile -
